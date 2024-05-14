7c99691ba3354a279a310976f219c6015dd7cb07
jisupark1992 <jisupark1992@gmail.com>
2024-05-15 07:25:08 +0900

    [feat] 이벤트처리와 useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import Profile from './components/Profile'
 import './App.css'
 import PackingList from './components/PackingList'
+import BasicButton from "./components/BasicButton.jsx";
+import UseRefHookDemo from "./components/UseRefHookDemo.jsx";
 
 export default function App() {
+    const buttonClicked = () => {
+        alert("hi");
+    };
 
   return (
-    <>
-      <Profile/>
-      <PackingList/>
-    </>
+      <>
+          <Profile/>
+          <hr/>
+          <PackingList/>
+          <BasicButton onClick={buttonClicked}>클릭해주세요</BasicButton>
+          <hr/>
+          <div className="Toolbar" onClick={() => alert('you clicked on the toolbar')}>
+              <BasicButton onClick={() =>
+                  alert('Playing!')
+              }>Play Movie</BasicButton>
+              <BasicButton onClick={() =>
+                  alert('Uploading!')
+              }>Upload Image</BasicButton>
+          </div>
+          <hr/>
+          <UseRefHookDemo></UseRefHookDemo>
+      </>
   )
 }
